56bd8846cb56e1badcd05ed7ce0227bf8546564f
daon1213 <diaeiyo1213@naver.com>
2021-11-20 22:32:35 +0900

    first commit!

---
+++ app/src/main/java/com/koddev/instagramtest/Adapter/MyFotosAdapter.java
@@ -0,0 +1,76 @@
+package com.koddev.instagramtest.Adapter;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.koddev.instagramtest.Fragments.PostDetailFragment;
+import com.koddev.instagramtest.Model.Post;
+import com.koddev.instagramtest.R;
+
+import java.util.List;
+
+import static android.content.Context.MODE_PRIVATE;
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyFotosAdapter extends RecyclerView.Adapter<MyFotosAdapter.ImageViewHolder> {
+
+    private Context mContext;
+    private List<Post> mPosts;
+
+    public MyFotosAdapter(Context context, List<Post> posts){
+        mContext = context;
+        mPosts = posts;
+    }
+
+    @Override
+    public MyFotosAdapter.ImageViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.fotos_item, parent, false);
+        return new MyFotosAdapter.ImageViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyFotosAdapter.ImageViewHolder holder, final int position) {
+
+        final Post post = mPosts.get(position);
+
+        Glide.with(mContext).load(post.getPostimage()).into(holder.post_image);
+
+        holder.post_image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = mContext.getSharedPreferences("PREFS", MODE_PRIVATE).edit();
+                editor.putString("postid", post.getPostid());
+                editor.apply();
+
+                ((FragmentActivity)mContext).getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PostDetailFragment()).commit();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPosts.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView post_image;
+
+
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+
+            post_image = itemView.findViewById(R.id.post_image);
+
+        }
+    }
+}
